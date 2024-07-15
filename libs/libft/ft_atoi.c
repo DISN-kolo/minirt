892a1e54b2121b5966dcd4e093af840a6d1a91a0
Artem Kozin <akozin@car6s1.42barcelona.com>
2024-07-15 15:48:27 +0200

    atoi simplified for our needs and main header 42 added

--- libs/libft/ft_atoi.c
+++ libs/libft/ft_atoi.c
@@ -6,58 +6,35 @@
 /*   By: akozin <akozin@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:07:12 by akozin            #+#    #+#             */
-/*   Updated: 2024/01/13 13:47:50 by akozin           ###   ########.fr       */
+/*   Updated: 2024/07/15 15:46:37 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-static int	ft_isspace(char x)
-{
-	return (x == '\t' || x == '\n' || x == '\v' || x == '\f' || x == '\r'
-		|| x == ' ');
-}
 
 static int	ft_isn(char x)
 {
 	return (x >= '0' && x <= '9');
 }
 
-static int	ft_issign(char x)
+int	ft_atoi(char *src)
 {
-	return (x == '-' || x == '+');
-}
+	int	res;
 
-static int	twenty_fiver(int *sixn, char *str)
-{
-	while (str[sixn[1]])
+	res = 0;
+	while (*src && *src != '\n')
 	{
-		if (ft_isspace(str[sixn[1]]) && (sixn[2] <= 0))
-			sixn[2] = 0;
-		else if (ft_issign(str[sixn[1]]) && (sixn[2] <= 1))
-		{
-			sixn[2] = 2;
-			sixn[0] = 1 - (str[sixn[1]] == '-') * 2;
-		}
-		else if (ft_isn(str[sixn[1]]) && (sixn[2] <= 2))
-		{
-			sixn[2] = 2;
-			sixn[3] = sixn[3] * 10 + (str[sixn[1]] - '0');
-		}
+		res *= 10;
+		if (ft_isn(*src))
+			res += *src - '0';	
 		else
-			break ;
-		sixn[1]++;
+			return (-1);
+		src++;
 	}
-	return (sixn[3] * sixn[0]);
+	return (res);
 }
-
-/* x is state. -1 is starting, 0 is spaces, 1 is signs, 2 is numbers. */
-/* sixn is our magic array containing s, i, x, n */
-int	ft_atoi(char *str)
-{
-	int	sixn[4];
-
-	sixn[0] = 1;
-	sixn[1] = 0;
-	sixn[2] = -1;
-	sixn[3] = 0;
-	return (twenty_fiver(sixn, str));
-}
+// dead simple version for positive numbers only. spaces BEGONE
+// used since we have only 0..255 as ints
+/*
+	while (*src != '\n' && ft_isspace(*src))
+		src++;
+*/
+// spaces hello ^^^^^ just in case
