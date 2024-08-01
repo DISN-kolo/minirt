534990308a5b1e3bc1f6997468c6f3c7ce4fe6b1
Artem Kozin <akozin@car8s1.42barcelona.com>
2024-08-01 13:25:03 +0200

    final scenes i think and a resolution change heehee

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: fcosta-f < fcosta-f@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 16:07:24 by akozin            #+#    #+#             */
-/*   Updated: 2024/07/31 18:20:52 by akozin           ###   ########.fr       */
+/*   Updated: 2024/08/01 13:07:11 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@
 //# define WIN_X 1920.f
 //# define WIN_Y 270.f
 //# define WIN_X 480.f
-//# define WIN_Y 540.f
-//# define WIN_X 960.f
-# define WIN_Y 1000.f
-# define WIN_X 1000.f
+# define WIN_Y 540.f
+# define WIN_X 960.f
+//# define WIN_Y 1000.f
+//# define WIN_X 1000.f
 //# define WIN_X 150.f
 //# define WIN_Y 150.f
 # define FALLOFF 1.3f
